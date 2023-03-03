f253a9767e5e14365e7bba0338aff9d7194a9a53
Fabian <fabian6_03@hotmail.com>
2023-03-03 00:32:17 -0700

    Create new models and the http request to have the general information about the most popular or the current movies on a cinema

---
+++ lib/models/movie.dart
@@ -0,0 +1,82 @@
+import 'dart:convert';
+class Movie {
+    Movie({
+        required this.adult,
+        this.backdropPath,
+        required this.genreIds,
+        required this.id,
+        required this.originalLanguage,
+        required this.originalTitle,
+        required this.overview,
+        required this.popularity,
+        this.posterPath,
+        this.releaseDate,
+        required this.title,
+        required this.video,
+        required this.voteAverage,
+        required this.voteCount,
+    });
+
+    bool adult;
+    String? backdropPath;
+    List<int> genreIds;
+    int id;
+    String originalLanguage;
+    String originalTitle;
+    String overview;
+    double popularity;
+    String? posterPath;
+    String? releaseDate;
+    String title;
+    bool video;
+    double voteAverage;
+    int voteCount;
+
+    get fullPosterImg{
+      if(this.posterPath != null) return 'https://image.tmdb.org/t/p/w500${this.posterPath}';
+
+      return 'https://i.stack.imgur.com/GNhxO.png';
+    }
+
+    factory Movie.fromRawJson(String str) => Movie.fromJson(json.decode(str));
+
+    factory Movie.fromJson(Map<String, dynamic> json) => Movie(
+        adult             : json["adult"],
+        backdropPath      : json["backdrop_path"],
+        genreIds          : List<int>.from(json["genre_ids"].map((x) => x)),
+        id                : json["id"],
+        originalLanguage  : json["original_language"],
+        originalTitle     : json["original_title"],
+        overview          : json["overview"],
+        popularity        : json["popularity"]?.toDouble(),
+        posterPath        : json["poster_path"],
+        releaseDate       : json["release_date"],
+        title             : json["title"],
+        video             : json["video"],
+        voteAverage       : json["vote_average"]?.toDouble(),
+        voteCount         : json["vote_count"],
+    );
+
+    
+}
+
+// enum OriginalLanguage { EN, ES, NO, RU }
+
+// final originalLanguageValues = EnumValues({
+//     "en": OriginalLanguage.EN,
+//     "es": OriginalLanguage.ES,
+//     "no": OriginalLanguage.NO,
+//     "ru": OriginalLanguage.RU
+// });
+
+// class EnumValues<T> {
+//     Map<String, T> map;
+//     late Map<T, String> reverseMap;
+
+//     EnumValues(this.map);
+
+//     Map<T, String> get reverse {
+//         reverseMap = map.map((k, v) => MapEntry(v, k));
+//         return reverseMap;
+//     }
+// }
