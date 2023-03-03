ca91fab76e26512543ef81de03cceb344c11f2ea
Fabian <fabian6_03@hotmail.com>
2023-03-03 00:27:59 -0700

    Change widget parameters, now accepts a list of <Movie> and now it can display every single image for the current movie on cinema

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,32 +1,33 @@
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import 'package:movies/models/models.dart';
 
 class CardSwiperScreen extends StatelessWidget {
    
-  const CardSwiperScreen({super.key});
+  final List<Movie>movies;
+  const CardSwiperScreen({super.key, required this.movies});
   
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
     
     return Container(
       width: double.infinity,
       height: size.height * 0.5,
       child: Swiper(
-        itemCount: 10,
+        itemCount: movies.length,
         layout: SwiperLayout.STACK,
         itemWidth: size.width * 0.6,
         itemHeight: size.height * 0.43,
         itemBuilder: (context, index) {
-          
+
           return  GestureDetector(
             onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-instance'),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20),
               child: FadeInImage(
                 placeholder: AssetImage('assets/no-image.jpg'), 
-                image: NetworkImage('https://via.placeholder.com/300x400'),
+                image: NetworkImage(movies[index].fullPosterImg),
                 fit: BoxFit.cover,
               ),
             ),
