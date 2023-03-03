12c6d5c58c41494163a14eaca5b72b50d7bb5128
Fabian <fabian6_03@hotmail.com>
2023-03-03 00:17:00 -0700

    Add new stateless widget to handle the app providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:movies/providers/movies_provider.dart';
 import 'package:movies/routes/app_routes.dart';
-import 'package:movies/screens/screens.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => MoviesProvider(), lazy: false,)
+      ],
+      child: MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Movies',
